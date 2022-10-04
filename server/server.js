7c15d2584475bb3b00557ec749e8d1e4aa06af2c
Code Cademy <dangkimberly26@gmail.com>
2022-10-04 14:08:08 -0700

    get request

--- server/server.js
+++ server/server.js
@@ -6,4 +6,17 @@ const PORT = 1996;
 
 app.use(cors());
 
+app.get('/', (req, res) =>{
+    res.json("Weather App Information")
+})
+
+app.get('/api/weather', (req, res) => {
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=irvine&APPID=${process.env.REACT_APP_API}&units=metric}`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      res.send(data);
+    });
+  });
+
 app.listen(PORT, () => console.log(`Hola! Server is running on port ${PORT}`));
