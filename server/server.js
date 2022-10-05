46bb4fd1bc85d4af6f0d8aa9e98abe421dea5c07
Code Cademy <dangkimberly26@gmail.com>
2022-10-05 11:37:59 -0700

    added fetch api

--- server/server.js
+++ server/server.js
@@ -1,18 +1,25 @@
 import express from 'express';
 import cors from 'cors';
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+dotenv.config()
 
 const app = express();
 const PORT = 1996;
 
 app.use(cors());
 
+console.log(`Your api key is ${process.env.REACT_APP_API_KEY}`);
+
 app.get('/', (req, res) =>{
     res.json("Weather App Information")
 })
 
+let URL = `https://api.openweathermap.org/data/2.5/weather?zip=92111,us&APPID=${process.env.REACT_APP_API_KEY}&units=imperial`;
+console.log(process.env.REACT_APP_API_KEY, "API KIMBERLY")
+
 app.get('/api/weather', (req, res) => {
-    const url=`https://api.openweathermap.org/data/2.5/weather?q=irvine&APPID=${process.env.REACT_APP_API}&units=metric}`;
-    fetch(url)
+    fetch(URL)
     .then((response) => response.json())
     .then((data) => {
       res.send(data);
